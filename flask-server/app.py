ca17e50b523e233a1c8978656e839dd338bac0c7
ChenQirui1 <ivanchen.code@gmail.com>
2023-01-31 00:04:03 +0800

    Server - Added templates and logic

--- flask-server/app.py
+++ flask-server/app.py
@@ -16,28 +16,8 @@ video = cv2.VideoCapture(0)
 @app.route("/")
 def hello():
     
-    # with open('pipeline_config.yml') as f:
-    #     try:
-    #         os.system("peekingduck run")
-    #         #print(dict)
-    #     except:
-    #         print("Fail")
-            
-            
-    # os.system("peekingduck run")
-        
-    # return "hello_world"
-    return "Default Message"
-
-# def gen(video):
-#     while True:
-#         success, image = video.read()
-#         ret, jpeg = cv2.imencode('.jpg', image)
-#         frame = jpeg.tobytes()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-           
+    return render_template('home.html')
+ 
 @app.route('/upload_video', methods=['GET','POST'])
 def upload_video():
     if request.method == 'POST':
@@ -56,10 +36,48 @@ def upload_video():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('camera_test', name=filename))
-    
-@app.route('/camera_test')
+
+
+@app.route('/upload_image', methods=['GET','POST'])
+def upload_image():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            #flash('No file part')
+            return redirect(url_for('camera_test'))
+        
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(url_for('camera_test'))
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('camera_test', name=filename))
+
+@app.route('/camera')
 def camera_test():
     return render_template('capture.html')
 
+@app.route('/display/<incident_id>')
+def display_content(incident_id):
+    
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2204, threaded=True,debug=False)
+    app.run(host='0.0.0.0', threaded=True,debug=True)
+    
+    
+# with open('pipeline_config.yml') as f:
+    #     try:
+    #         os.system("peekingduck run")
+    #         #print(dict)
+    #     except:
+    #         print("Fail")
+            
+            
+    # os.system("peekingduck run")
+        
+    # return "hello_world"
